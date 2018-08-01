5377cd514ad96b2c8044aad88b7a48ccad6b8898
sean1130 <39747545+sean1130@users.noreply.github.com>
2018-08-01 15:45:50 -0600

    setup app routes

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import { Field, reduxForm} from 'redux-form';
+
+import { withRouter } from 'react-router-dom';
 
 class SearchBar extends Component {
+
+    handleFormSubmit = function({query}) {
+        this.props.onSubmit(query);
+    }
+
+    renderInput(field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
-                <input placeholder="Search DailySmarty"/>
-               
+            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <div className="search-bar__wrapper">
+                    <Field name="query" component={this.renderInput}/>
+                </div>
             </form>
-
-
-
         )
     }
 }
+
+SearchBar = reduxForm({
+    form: 'searchBar'
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
